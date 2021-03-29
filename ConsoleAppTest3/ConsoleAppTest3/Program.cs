ba5ebf0eb10b520724992d3e85cfa68c8f5bc4e7
1020681 <70630991+1020681@users.noreply.github.com>
2021-03-29 10:37:02 +0200

    merge solution test 3

---
+++ ConsoleAppTest3/ConsoleAppTest3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
