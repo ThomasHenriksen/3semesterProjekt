b58970f89a8513a898240676494df77312140535
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-08 09:48:48 +0200

    ProductAccess and ProductDatabaseAccess has been updated

---
+++ ArmysalgService/SpikeProductData/DatabaseLayer/IProductAccess.cs
@@ -0,0 +1,18 @@
+﻿using SpikeProductData.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpikeProductData.DatabaseLayer
+{
+    public interface IProductAccess
+    {
+        Product GetProductById(int id);
+        List<Product> GetProductAll();
+        int CreateProduct(Product productToAdd);
+        bool UpdateProduct(Product productToUpdate);
+        bool DeleteProductById(int id);
+    }
+}
---
+++ ArmysalgService/SpikeProductData/DatabaseLayer/ProductDatabaseAccess.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using SpikeProductData.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SpikeProductData.DatabaseLayer
+{
+    public class ProductDatabaseAccess : IProductAccess
+    {
+        readonly string _connectionString;
+
+        public ProductDatabaseAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ArmysalgConnection");
+        }
+        public int CreateProduct(Product productToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(Product productToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
