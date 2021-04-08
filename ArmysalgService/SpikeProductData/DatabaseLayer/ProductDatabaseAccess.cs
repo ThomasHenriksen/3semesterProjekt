bbb4f8e969f53652ef10bc2ed4ec6b706351a184
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-08 10:20:14 +0200

    Create getAllproduct method

--- ArmysalgService/SpikeProductData/DatabaseLayer/ProductDatabaseAccess.cs
+++ ArmysalgService/SpikeProductData/DatabaseLayer/ProductDatabaseAccess.cs
@@ -29,7 +29,27 @@ namespace SpikeProductData.DatabaseLayer
 
         public List<Product> GetProductAll()
         {
-            throw new NotImplementedException();
+            List<Product> foundProducts;
+            Product readProduct;
+
+            string queryString = "select productNo, name, description, purchasePrice, status, stock, minStock, maxStock, isDeleted from Product";
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            using (SqlCommand readCommand = new SqlCommand(queryString, con))
+            {
+                con.Open();
+
+                SqlDataReader productReader = readCommand.ExecuteReader();
+
+                foundProducts = new List<Product>();
+                while (productReader.Read())
+                {
+                    readProduct = GetProductFromReader(productReader);
+                    foundProducts.Add(readProduct);
+                }
+            }
+            return foundProducts;
+           
         }
 
         public Product GetProductById(int id)
@@ -40,6 +60,29 @@ namespace SpikeProductData.DatabaseLayer
         public bool UpdateProduct(Product productToUpdate)
         {
             throw new NotImplementedException();
+        }
+
+        private Product GetProductFromReader(SqlDataReader productReader)
+        {
+            Product foundProduct;
+            int tempId, tempStock, tempMinStock, tempMaxStock;
+            string tempName, tempDescription, tempStatus;
+            decimal tempPurchasePrice;
+            bool tempIsDeleted;
+
+            tempId = productReader.GetInt32(productReader.GetOrdinal("productNo"));
+            tempName = productReader.GetString(productReader.GetOrdinal("name"));
+            tempDescription = productReader.GetString(productReader.GetOrdinal("description"));
+            tempPurchasePrice = productReader.GetDecimal(productReader.GetOrdinal("purchasePrice"));
+            tempStatus = productReader.GetString(productReader.GetOrdinal("status"));
+            tempStock = productReader.GetInt32(productReader.GetOrdinal("stock"));
+            tempMinStock = productReader.GetInt32(productReader.GetOrdinal("minStock"));
+            tempMaxStock = productReader.GetInt32(productReader.GetOrdinal("maxStoct"));
+            tempIsDeleted = productReader.GetBoolean(productReader.GetOrdinal("isDeleted"));
+
+            foundProduct = new Product(tempId, tempName, tempDescription, tempPurchasePrice, tempStatus, tempStock, tempMinStock, tempMaxStock, tempIsDeleted);
+
+            return foundProduct;
         }
     }
 }
