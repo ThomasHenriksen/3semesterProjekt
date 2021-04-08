5362a89fcd4389879bc6eb507137bc70d6ed1a0a
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-08 12:37:20 +0200

    Controller create

---
+++ ArmysalgService/ArmysalgService/BusinesslogicLayer/ProductdataControl.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using SpikeProductData.DatabaseLayer;
+using SpikeProductData.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.BusinesslogicLayer
+{
+    public class ProductdataControl : IProductdata
+    {
+        IProductAccess _productAccess;
+
+        public ProductdataControl(IConfiguration inConfiguration)
+        {
+            _productAccess = new ProductDatabaseAccess(inConfiguration);
+        }
+
+        public int Add(Product newProduct)
+        {
+            int insertedId;
+            try {
+                insertedId = _productAccess.CreateProduct(newProduct);
+            } catch {
+                insertedId = -1;
+            }
+            return insertedId;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int idToMatch)
+        {
+            Product foundProduct;
+            try {
+                foundProduct = _productAccess.GetProductById(idToMatch);
+            } catch {
+                foundProduct = null;
+            }
+            return foundProduct;
+        }
+
+        public List<Product> Get()
+        {
+            List<Product> foundProducts;
+            try {
+                foundProducts = _productAccess.GetProductAll();
+            } catch {
+                foundProducts = null;
+            }
+            return foundProducts;
+        }
+
+        public bool Put(Product productToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArmysalgService/ArmysalgService/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using ArmysalgService.BusinesslogicLayer;
+using ArmysalgService.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.Controllers
+{
+    [Route("api/persons")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductdataControl _pControl;
+        private readonly IConfiguration _configuration;
+
+        public ProductController(IConfiguration inConfiguration)
+        {
+            _configuration = inConfiguration;
+            _pControl = new ProductdataControl(_configuration);
+        }
+
+        // URL: api/products
+        [HttpGet]
+        public ActionResult<List<ProductdataReadDto>> Get()
+        {
+            return null;
+        }
+
+        // URL: api/products/{id}
+        [HttpGet, Route("{id}")]
+        public ActionResult<ProductdataReadDto> Get(int id)
+        {
+            return null;
+        }
+
+        // URL: api/persons
+        [HttpPost]
+        public ActionResult PostNewProduct(ProductdataCreateDto inProduct)
+        {
+            return null;
+        }
+
+    }
+}
