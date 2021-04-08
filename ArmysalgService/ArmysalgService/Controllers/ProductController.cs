8dbbea863bbe56b4e2a3bb39c74fbb79bf3377dd
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-08 13:08:43 +0200

    connection from api to database

--- ArmysalgService/ArmysalgService/Controllers/ProductController.cs
+++ ArmysalgService/ArmysalgService/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using ArmysalgService.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using SpikeProductData.ModelLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,11 +23,35 @@ namespace ArmysalgService.Controllers
             _pControl = new ProductdataControl(_configuration);
         }
 
+
+        /* Data retrieved from the database, is converted to eksternal 
+         * format, evaluated and the an response must be sent back*/
         // URL: api/products
         [HttpGet]
         public ActionResult<List<ProductdataReadDto>> Get()
         {
-            return null;
+            ActionResult<List<ProductdataReadDto>> foundReturn;
+            // retrieve and convert data
+            List<Product> foundProducts = _pControl.Get();
+            List<ProductdataReadDto> foundDts = ModelConversion.ProductdataReadDtoConvert.FromProductCollection(foundProducts);
+            // evaluate
+            if (foundDts != null)
+            {
+                if (foundDts.Count > 0)
+                {
+                    foundReturn = Ok(foundDts);         //Statuscode 200
+                }
+                else
+                {
+                    foundReturn = new StatusCodeResult(204);    //Ok, but no content
+                }
+            }
+            else
+            {
+                foundReturn = new StatusCodeResult(500);        //Server error
+            }
+            // send response back to client
+            return foundReturn;
         }
 
         // URL: api/products/{id}
