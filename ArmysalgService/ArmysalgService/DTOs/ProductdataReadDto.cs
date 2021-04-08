d213282a7f3bc0d15103561025ff352e01ca46b6
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-08 11:56:58 +0200

    Dto and Coversion created

---
+++ ArmysalgService/ArmysalgService/DTOs/ProductdataReadDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.DTOs
+{
+    public class ProductdataReadDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string Status { get; set; }
+        public int Stock { get; set; }
+        public int MinStock { get; set; }
+        public int MaxStock { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ProductdataReadDto()
+        {
+        }
+
+        public ProductdataReadDto(string name, string description, decimal purchasePrice, string status, int stock, int minStock, int maxStock, bool isDeleted)
+        {
+            Name = name;
+            Description = description;
+            PurchasePrice = purchasePrice;
+            Status = status;
+            Stock = stock;
+            MinStock = minStock;
+            MaxStock = maxStock;
+            IsDeleted = isDeleted;
+        }
+    }
+}
---
+++ ArmysalgService/ArmysalgService/ModelConversion/ProductdataReadDtoConvert.cs
@@ -0,0 +1,38 @@
+﻿using ArmysalgService.DTOs;
+using SpikeProductData.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.ModelConversion
+{
+    public class ProductdataReadDtoConvert
+    {
+        public static List<ProductdataReadDto> FromProductCollection(List<Product> inProducts)
+        {
+            List<ProductdataReadDto> aProductReadDtoList = null;
+            if (inProducts != null)
+            {
+                aProductReadDtoList = new List<ProductdataReadDto>();
+                ProductdataReadDto tempDto;
+                foreach (Product aProduct in inProducts)
+                {
+                    tempDto = FromProduct(aProduct);
+                    aProductReadDtoList.Add(tempDto);
+                }
+            }
+            return aProductReadDtoList;
+        }
+
+        private static ProductdataReadDto FromProduct(Product inProduct)
+        {
+            ProductdataReadDto aProductReadDto = null;
+            if (inProduct != null)
+            {
+                aProductReadDto = new ProductdataReadDto(inProduct.Name, inProduct.Description, inProduct.PurchasePrice, inProduct.Status, inProduct.Stock, inProduct.MinStock, inProduct.MaxStock, inProduct.IsDeleted);
+            }
+            return aProductReadDto;
+        }
+    }
+}
