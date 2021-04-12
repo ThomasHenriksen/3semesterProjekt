8411e2208bef9a89180074c20d98e837e4eb3e0c
LauridsT <70630784+LauridsT@users.noreply.github.com>
2021-04-12 14:29:12 +0200

    GUI View all products

--- ArmysalgClientDesktop/ArmysalgClientDesktop/GuiLayer/Form1.cs
+++ ArmysalgClientDesktop/ArmysalgClientDesktop/GuiLayer/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArmysalgClientDesktop.ControlLayer;
+using ArmysalgClientDesktop.ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,40 @@ namespace ArmysalgClientDesktop
 {
     public partial class Form1 : Form
     {
+
+        ProductControl productController;
+
         public Form1()
         {
             InitializeComponent();
+            productController = new();
         }
 
+        private async void btnClickGetAllProducts_Click(object sender, EventArgs e)
+        {
+            List<Product> products = await productController.GetAllProducts();
+            List<String> nameOfProducts = new();
+            foreach(var p in products)
+            {
+                nameOfProducts.Add(p.Name);
+            }
+            listBox1.DataSource = nameOfProducts;
+        }
+
+        private void btnClickAddProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //public async Task<List<string>> CreateListOfProducts()
+        //{
+        //    List<String> namesOfProducts = new List<String>();
+        //    List<Product> productsFound = await productController.GetAllProducts();
+        //    foreach (var p in productsFound)
+        //    {
+        //        namesOfProducts.Add(p.Name);
+        //    }
+        //    return namesOfProducts;
+        //}
     }
 }
