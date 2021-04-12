6c85401ce3539635f66d6fb88b94bac361eda406
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-12 11:36:33 +0200

    product controller

---
+++ ArmysalgClientDesktop/ArmysalgClientDesktop/ControlLayer/ProductControl.cs
@@ -0,0 +1,28 @@
+﻿using ArmysalgClientDesktop.ModelLayer;
+using ArmysalgClientDesktop.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmysalgClientDesktop.ControlLayer
+{
+    public class ProductControl
+    {
+        ProductServiceAccess _pAccess;
+
+        public ProductControl()
+        {
+            _pAccess = new ProductServiceAccess();
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            List<Product> foundProducts = await _pAccess.GetProducts();
+            return foundProducts;
+        }
+
+        public async Task<int> SaveProduct(string )
+    }
+}
