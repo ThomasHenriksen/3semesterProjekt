3584e89c0309dcf2e0b96c47b5cc5a7b4a5decd5
LauridsT <70630784+LauridsT@users.noreply.github.com>
2021-04-14 11:58:43 +0200

    UnitTest - CreateEmployee

---
+++ ArmysalgService/ProductDataTest/CreateEmployeeUnitTest.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using Xunit.Abstractions;
+using SpikeProductData.DatabaseLayer;
+using SpikeProductData.ModelLayer;
+
+namespace ArmysalgDataTest
+{
+    public class TestCreateEmployee
+    {
+        private readonly ITestOutputHelper output;
+        readonly private IEmployeeAccess _employeeAccess;
+        readonly string _connectionString = "Server = hildur.ucn.dk; Database = dmaa0220_1085014; User Id = dmaa0220_1085014; Password = Password1!; Trusted_Connection = False";
+
+        public TestCreateEmployee(ITestOutputHelper output)
+        {
+            this.extraOutput = output;
+            _employeeAccess = new EmployeeDatabaseAccess(_connectionString);
+        }
+
+        [Fact]
+        public void CreateCustomerTest()
+        {
+            //Arrange
+            string firstName = "Larskovic";
+            string lastName = "35 lol";
+            string address = "Oldstreet 1986";
+            string zipCode = "9400";
+            string city = "Nørresundby";
+            string phone = "35353535";
+            string email = "livefastdiemiddleage@onlyfans.com";
+            int employeeNo = 1337;
+            double salary = 350000;
+            string position = "Swaggy";
+            //Act
+            Employee employeeToCreate = new(firstName, lastName, address, zipCode, city, phone, email, employeeNo, salary, position);
+            _employeeAccess.CreateEmployee(employeeToCreate);
+            Employee readEmployee = _employeeAccess.GetEmployeeByID(1337);
+            //Assert
+            Assert.Equal(employeeToCreate, readEmployee);
+        }
+    }
+}
