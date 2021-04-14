f5e97039ac751ba375a00f35ed86f8250f8b90f9
LauridsT <70630784+LauridsT@users.noreply.github.com>
2021-04-14 12:20:28 +0200

    Update Person.cs

--- ArmysalgService/SpikeProductData/ModelLayer/Person.cs
+++ ArmysalgService/SpikeProductData/ModelLayer/Person.cs
@@ -8,23 +8,23 @@ namespace ArmysalgDataAccess.ModelLayer
 {
     public class Person
     {
-        public String firstName { get; set; }
-        public String lastName { get; set; }
-        public String address { get; set; }
-        public String zipCode { get; set; }
-        public String city { get; set; }
-        public String phone { get; set; }
-        public String email { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public String ZipCode { get; set; }
+        public String City { get; set; }
+        public String Phone { get; set; }
+        public String Email { get; set; }
 
         public Person(String firstName, String lastName, String address, String zipCode, String city, String phone, String email) 
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.zipCode = zipCode;
-            this.city = city;
-            this.phone = phone;
-            this.email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            ZipCode = zipCode;
+            City = city;
+            Phone = phone;
+            Email = email;
         }
     }
 }
