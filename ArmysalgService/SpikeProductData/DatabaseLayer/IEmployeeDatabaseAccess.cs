e83cb1ff8593186331fdb3188a0f3e88992010a4
LauridsT <70630784+LauridsT@users.noreply.github.com>
2021-04-15 11:24:29 +0200

    Update IEmployeeDatabaseAccess.cs

--- ArmysalgService/SpikeProductData/DatabaseLayer/IEmployeeDatabaseAccess.cs
+++ ArmysalgService/SpikeProductData/DatabaseLayer/IEmployeeDatabaseAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArmysalgDataAccess.ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace ArmysalgDataAccess.DatabaseLayer
 {
-    class IEmployeeDatabaseAccess
+    public interface IEmployeeDatabaseAccess
     {
+        Employee GetEmployeeByCustomerNo(int employeeNo);
+        int CreateEmployee(Employee employeeToAdd);
+        bool DeleteEmployeeByCustomerNo(int employeeNo);
     }
 }
