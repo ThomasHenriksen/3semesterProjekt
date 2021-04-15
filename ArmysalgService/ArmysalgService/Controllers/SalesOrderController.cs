c763367e21286c3f9c48923151c96a12d67fa84e
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-15 12:01:47 +0200

    salesOrderController addet

---
+++ ArmysalgService/ArmysalgService/Controllers/SalesOrderController.cs
@@ -0,0 +1,26 @@
+﻿using ArmysalgService.BusinesslogicLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.Controllers
+{
+    [Route("api/salesOrders")]
+    [ApiController]
+    public class SalesOrderController : ControllerBase
+    {
+        private readonly SalesOrderdataControl _sControl;
+        private readonly IConfiguration _configuration;
+
+        public SalesOrderController(IConfiguration inConfiguration)
+        {
+            _configuration = inConfiguration;
+            _sControl = new SalesOrderdataControl(_configuration);
+        }
+
+
+    }
+}
