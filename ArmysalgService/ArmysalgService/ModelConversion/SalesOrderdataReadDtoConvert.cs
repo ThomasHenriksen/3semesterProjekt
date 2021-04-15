f6e0881f0cb855acdc1241592a24ab815eed3d5c
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-15 11:18:43 +0200

    readDtoConvert class

---
+++ ArmysalgService/ArmysalgService/ModelConversion/SalesOrderdataReadDtoConvert.cs
@@ -0,0 +1,39 @@
+﻿using ArmysalgDataAccess.ModelLayer;
+using ArmysalgService.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.ModelConversion
+{
+    public class SalesOrderdataReadDtoConvert
+    {
+        public static List<SalesOrderdataReadDto> FromSalesOrderCollection(List<SalesOrder> inSalesOrders)
+        {
+            List<SalesOrderdataReadDto> aSalesOrderReadDtoList = null;
+            if (inSalesOrders != null)
+            {
+                aSalesOrderReadDtoList = new List<SalesOrderdataReadDto>();
+                SalesOrderdataReadDto tempDto;
+                foreach (SalesOrder aSalesOrder in inSalesOrders)
+                {
+                    tempDto = FromSalesOrder(aSalesOrder);
+                    aSalesOrderReadDtoList.Add(tempDto);
+                }
+            }
+                return aSalesOrderReadDtoList;
+            }
+
+            public static SalesOrderdataReadDto FromSalesOrder(SalesOrder inSalesOrder)
+            {
+                SalesOrderdataReadDto aSalesOrderReadDto = null;
+                if (inSalesOrder != null)
+                {
+                    aSalesOrderReadDto = new SalesOrderdataReadDto(inSalesOrder.SalesNo, inSalesOrder.SalesDate, inSalesOrder.PaymentAmount, inSalesOrder.Status, inSalesOrder.SalesLineItem);
+                }
+                return aSalesOrderReadDto;
+            }
+        }
+    }
+}
