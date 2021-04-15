f7d2501cf68eaad9b32ec88b22f4a0ee91e421de
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-15 10:50:14 +0200

    sales order dtos created

---
+++ ArmysalgService/ArmysalgService/DTOs/SalesOrderdataWriteDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgService.DTOs
+{
+    public class SalesOrderdataWriteDto
+    {
+        public int SalesNo { get; set; }
+        public DateTime SalesDate { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public string Status { get; set; }
+        public int SalesLineItem { get; set; }
+        public int ShippingId { get; set; }
+        public int EmployeeId { get; set; }
+        public int CustomerId { get; set; }
+
+        public SalesOrderdataWriteDto(int salesNo, DateTime salesDate, decimal paymentAmount, string status, int salesLineItem)
+        {
+            SalesNo = salesNo;
+            SalesDate = salesDate;
+            PaymentAmount = paymentAmount;
+            Status = status;
+            SalesLineItem = salesLineItem;
+        }
+
+        public SalesOrderdataWriteDto(int salesNo, DateTime salesDate, decimal paymentAmount, string status, int salesLineItem, int shippingId, int employeeId, int customerId) : this(salesNo, salesDate, paymentAmount, status, salesLineItem)
+        {
+            ShippingId = shippingId;
+            EmployeeId = employeeId;
+            CustomerId = customerId;
+        }
+    }
+}
