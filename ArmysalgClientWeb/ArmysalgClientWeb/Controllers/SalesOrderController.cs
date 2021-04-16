69a9f9f72ea68f89eff24aab5917543595aceb04
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-16 13:59:13 +0200

    Created SalesOrderController

---
+++ ArmysalgClientWeb/ArmysalgClientWeb/Controllers/SalesOrderController.cs
@@ -0,0 +1,21 @@
+﻿using ArmysalgClientWeb.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgClientWeb.Controllers
+{
+    public class SalesOrderController : Controller
+    {
+        readonly SalesOrderLogic _salesOrderLogic;
+
+        public SalesOrderController()
+        {
+            _salesOrderLogic = new SalesOrderLogic();
+        }
+
+
+    }
+}
