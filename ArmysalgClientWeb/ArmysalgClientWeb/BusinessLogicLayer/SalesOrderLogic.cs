e8fbab778a30090b5dd56b6c87c5f3b210fd9d8a
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-16 13:55:58 +0200

    create insert metode

--- ArmysalgClientWeb/ArmysalgClientWeb/BusinessLogicLayer/SalesOrderLogic.cs
+++ ArmysalgClientWeb/ArmysalgClientWeb/BusinessLogicLayer/SalesOrderLogic.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using ArmysalgClientWeb.Models;
+using ArmysalgClientWeb.ServiceLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ArmysalgClientWeb.BusinessLogic
 {
-    public class salesOrderLogic
+    public class SalesOrderLogic
     {
+        private readonly SalesOrderService _salesOrderService;
+
+        public SalesOrderLogic()
+        {
+            _salesOrderService = new SalesOrderService();
+        }
+
+        public async Task<int> InsertSalesOrder(SalesOrder salesOrder)
+        {
+            int wasInserted = -1;
+            try
+            {
+                wasInserted = await _salesOrderService.SaveSalesOrder(salesOrder);
+            }
+            catch
+            {
+                wasInserted = -1;
+            }
+            return wasInserted;
+        }
     }
 }
