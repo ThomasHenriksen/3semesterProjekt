94da267609732b87fb1c6c3924665298b1d47e5e
1083765 <70630961+1083765@users.noreply.github.com>
2021-04-16 13:23:34 +0200

    created businesslogicLayer, created salesOrderClass, created salesOrderService

--- ArmysalgClientWeb/ArmysalgClientWeb/ServiceLayer/SalesOrderService.cs
+++ ArmysalgClientWeb/ArmysalgClientWeb/ServiceLayer/SalesOrderService.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using ArmysalgClientWeb.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ArmysalgClientWeb.ServiceLayer
@@ -15,6 +18,38 @@ namespace ArmysalgClientWeb.ServiceLayer
         public SalesOrderService()
         {
             _client = new HttpClient();
+        }
+
+        public async Task<int> SaveSalesOrder(SalesOrder salesOrderToSave)
+        {
+            int insertedSalesOrderNo;
+            string useRestUrl = restUrl;
+            var uri = new Uri(String.Format(useRestUrl, string.Empty));
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(salesOrderToSave);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                response = await _client.PostAsync(uri, content);
+                string resultSalesNo = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Int32.TryParse(resultSalesNo, out insertedSalesOrderNo);
+                }
+                else
+                {
+                    insertedSalesOrderNo = -2;
+                }
+            }
+            catch 
+            {
+                insertedSalesOrderNo = -3;
+            }
+
+            return insertedSalesOrderNo;
         }
     }
 }
