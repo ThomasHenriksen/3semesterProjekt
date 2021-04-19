394dc52217babe58ba472e20016cac704d4d38c1
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2021-04-19 10:04:18 +0200

    Customer

--- ArmysalgClientWeb/ArmysalgClientWeb/Controllers/CustomerController.cs
+++ ArmysalgClientWeb/ArmysalgClientWeb/Controllers/CustomerController.cs
@@ -1,5 +1,7 @@
-﻿using ArmysalgClientWeb.Models;
-using ArmysalgClientWeb.ServiceLayer;
+﻿using ArmysalgClientWeb.BusinessLogicLayer;
+using ArmysalgClientWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,85 @@ using System.Threading.Tasks;
 
 namespace ArmysalgClientWeb.Controllers
 {
-    public class CustomerController
+    public class CustomerController : Controller
     {
-     
+
+        private CustomerLogic _cmdAcces;
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            _cmdAcces = new CustomerLogic();
+            IEnumerable<Customer> allCustomers = (IEnumerable<Customer>)_cmdAcces.GetAllCustomer();
+            return View();
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
