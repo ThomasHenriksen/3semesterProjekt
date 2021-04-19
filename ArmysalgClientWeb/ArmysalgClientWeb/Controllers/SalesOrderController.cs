1499c432c738bb3aa7259b432e56b32301b6a9e7
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-19 13:42:21 +0200

    salesorder create version 2

--- ArmysalgClientWeb/ArmysalgClientWeb/Controllers/SalesOrderController.cs
+++ ArmysalgClientWeb/ArmysalgClientWeb/Controllers/SalesOrderController.cs
@@ -1,42 +1,94 @@
-﻿using ArmysalgClientWeb.BusinessLogic;
-using ArmysalgClientWeb.Models;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArmysalgClientWeb.BusinessLogicLayer;
+using ArmysalgClientWeb.Models;
+using ArmysalgClientWeb.BusinessLogic;
 
 namespace ArmysalgClientWeb.Controllers
 {
     public class SalesOrderController : Controller
     {
-        //readonly SalesOrderLogic _salesOrderLogic;
-
-        public SalesOrderController()
+        private SalesOrderLogic _salesOrderLogic;
+        // GET: SalesOrderController1
+        public ActionResult Index()
         {
-            //_salesOrderLogic = new SalesOrderLogic();
+            return View();
         }
 
-        //[HttpPost]
-        //public async Task<ActionResult> Save(SalesOrder orderToSave)
-        //{
-        //    try
-        //    {
-        //        SalesOrder salesSave = new SalesOrder(orderToSave.SalesDate, orderToSave.PaymentAmount, orderToSave.Status, orderToSave.SalesLineItem);
-        //        int wasOk = await _salesOrderLogic.InsertSalesOrder(salesSave);
+        // GET: SalesOrderController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
 
-        //        if (wasOk > 0)
-        //        {
-        //            TempData["ProcessText"] = "Error inserting salesOrder";
-        //        }
-        //        return null; // HER MANGLER NOGET -- Måske ikke, måske kun hvis vi vil se den nye order
-        //    }
-        //    catch 
-        //    {
-        //        TempData["ProcessText"] = "Error in server!";
-        //        return View();
-        //    }
-        //}
+        // GET: SalesOrderController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
+        //// POST: SalesOrderController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(SalesOrder inSalesOrder)
+        {
+            try
+            {
+                _salesOrderLogic = new SalesOrderLogic();
+               int wasOk = await _salesOrderLogic.InsertSalesOrder(inSalesOrder);       
+
+                return RedirectToAction();
+            }
+            catch
+            {                
+                return View();
+            }
+        }
+
+        // GET: SalesOrderController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SalesOrderController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SalesOrderController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SalesOrderController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
