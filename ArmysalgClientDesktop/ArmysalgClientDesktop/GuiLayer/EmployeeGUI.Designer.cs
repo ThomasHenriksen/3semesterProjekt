c70f1a2f4eccddb262ab935186914d01d6b2785d
LauridsT <70630784+LauridsT@users.noreply.github.com>
2021-04-20 10:28:56 +0200

    GUI

---
+++ ArmysalgClientDesktop/ArmysalgClientDesktop/GuiLayer/EmployeeGUI.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ArmysalgClientDesktop.GuiLayer
+{
+    partial class EmployeeGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Employees = new System.Windows.Forms.Label();
+            this.EmployeeByID = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Employees
+            // 
+            this.Employees.AutoSize = true;
+            this.Employees.Location = new System.Drawing.Point(64, 29);
+            this.Employees.Name = "Employees";
+            this.Employees.Size = new System.Drawing.Size(38, 15);
+            this.Employees.TabIndex = 0;
+            this.Employees.Text = "label1";
+            // 
+            // EmployeeByID
+            // 
+            this.EmployeeByID.AutoSize = true;
+            this.EmployeeByID.Location = new System.Drawing.Point(433, 26);
+            this.EmployeeByID.Name = "EmployeeByID";
+            this.EmployeeByID.Size = new System.Drawing.Size(38, 15);
+            this.EmployeeByID.TabIndex = 1;
+            this.EmployeeByID.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(68, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(435, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(560, 78);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 4;
+            this.Search.Text = "button1";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(436, 154);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(100, 23);
+            this.firstName.TabIndex = 5;
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.EmployeeByID);
+            this.Controls.Add(this.Employees);
+            this.Name = "Employee";
+            this.Text = "Employee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Employees;
+        private System.Windows.Forms.Label EmployeeByID;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.TextBox firstName;
+    }
+}
---
+++ ArmysalgClientDesktop/ArmysalgClientDesktop/GuiLayer/EmployeeGUI.cs
@@ -0,0 +1,35 @@
+﻿using ArmysalgClientDesktop.ControlLayer;
+using ArmysalgClientDesktop.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArmysalgClientDesktop.GuiLayer
+{
+    public partial class EmployeeGUI : Form
+    {
+        EmployeeControl employeeControl;
+        public EmployeeGUI()
+        {
+            InitializeComponent();
+            this.employeeControl = new();
+        }
+
+        private async void Search_Click(object sender, EventArgs e)
+        {
+            int idFromTextBox;
+            Employee foundEmployee = null;
+            if (int.TryParse(textBox1.Text, out idFromTextBox))
+            {
+                foundEmployee = await employeeControl.GetEmployee(idFromTextBox);
+            }
+            firstName.Text = foundEmployee.FirstName;
+        }
+    }
+}
