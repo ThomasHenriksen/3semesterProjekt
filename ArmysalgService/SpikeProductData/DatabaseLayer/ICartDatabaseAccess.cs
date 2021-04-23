4109f162c7361ab76ea8f55954d06ba0daf6def3
1020681 <70630991+1020681@users.noreply.github.com>
2021-04-23 09:45:20 +0200

    Cart DatabaseAccess

---
+++ ArmysalgService/SpikeProductData/DatabaseLayer/ICartDatabaseAccess.cs
@@ -0,0 +1,16 @@
+﻿using ArmysalgDataAccess.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmysalgDataAccess.DatabaseLayer
+{
+    public interface ICartDatabaseAccess
+    {
+        int CreateCart(Cart cartToAdd);
+        Cart GetCartById(int cartId);
+        bool DeleteCartByCartId(int cartId);
+    }
+}
