be774bc50951f8fc4649ff198ab07e062a45905a
brianEngelbrektsen <70631178+brianEngelbrektsen@users.noreply.github.com>
2021-04-28 12:03:46 +0200

    Create SalesOrderStatus.cs

---
+++ ArmysalgService/SpikeProductData/Model/SalesOrderStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmysalgDataAccess.Model
+{
+    public enum SalesOrderStatus
+    { New, Hold, Shipped, Delivered, Closed }
+}
