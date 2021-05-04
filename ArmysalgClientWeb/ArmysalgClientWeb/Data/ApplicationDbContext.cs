3e497b5e180ea22a57d051d9f2f89845ee6162ff
1083765 <70630961+1083765@users.noreply.github.com>
2021-05-04 15:03:55 +0200

    WebClient show products

--- ArmysalgClientWeb/ArmysalgClientWeb/Data/ApplicationDbContext.cs
+++ ArmysalgClientWeb/ArmysalgClientWeb/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ArmysalgClientWeb.Models;
 
 namespace ArmysalgClientWeb.Data
 {
@@ -12,5 +13,6 @@ namespace ArmysalgClientWeb.Data
             : base(options)
         {
         }
+        public DbSet<ArmysalgClientWeb.Models.Product> Product { get; set; }
     }
 }
