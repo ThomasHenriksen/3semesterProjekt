7c51fae052f5e5066f51499ed29788cc23de0b67
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2021-05-04 13:36:05 +0200

    WEB PRODUCT

---
+++ ArmysalgClientWeb/ArmysalgClientWeb/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmysalgClientWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Category()
+        {
+
+        }
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
